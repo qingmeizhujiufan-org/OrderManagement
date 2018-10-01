be12a182b7fcf950904d5cdb8d1b82a825a66ddd
zhongzhenga <616028858@qq.com>
2018-10-01 22:39:43 +0800

    修改bug；

--- src/components/zzLeftSide/zzLeftSide.js
+++ src/components/zzLeftSide/zzLeftSide.js
@@ -13,167 +13,163 @@ const {Sider} = Layout;
 const SubMenu = Menu.SubMenu;
 
 class ZZLeftSide extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      defaultSelectedKeys: '',
-      authMenu: [],
-      subMenuList: null
-    };
-  }
-
-  componentWillMount = () => {
-    this.setAuthMenu();
-  }
-
-  componentDidMount = () => {
-    this.selectActiveTab();
-
-    window.addEventListener('hashchange', () => {
-      this.selectActiveTab();
-    });
-  }
-
-  componentWillReceiveProps = nextProps => {
-  }
-
-  setAuthMenu = () => {
-
-    if (sessionStorage.type !== undefined && sessionStorage.type !== null) {
-      const type = sessionStorage.type;
-
-      let authority_menu = [];
-      if (type === "1")
-        authority_menu = admin;
-      else if (type === "2") {
-        authority_menu = subAdmin;
-      }
-      else if (type === "3") {
-        authority_menu = operator;
-      }
-      let _menu = [];
-      menuTree.map(item => {
-        const _item = {};
-        for (let i = 0; i < authority_menu.length; i++) {
-          if (item.key === authority_menu[i].key) {
-            _item.key = item.key;
-            _item.iconType = item.iconType;
-            _item.label = item.label;
-            _item.children = [];
-            authority_menu[i].children.map(sub_key => {
-              _item.children.push(_.find(item.children, {key: sub_key}));
-            });
-            _menu.push(_item);
-          }
-        }
-      });
-      console.log("authMenu ===",_menu);
-      this.setState({authMenu: _menu});
+        this.state = {
+            defaultSelectedKeys: '',
+            authMenu: [],
+            subMenuList: null
+        };
     }
-  }
 
-  selectActiveTab = callback => {
-    const menu = this.getFlatMenu(this.state.authMenu);
-    const hashUrl = location.hash.split('#')[1];
-    for (let i = 0; i < menu.length; i++) {
-      const item = menu[i];
-      if (hashUrl.indexOf(item.link) > -1) {
-        this.setState({selectedKeys: item.key}, () => {
-          if (typeof  callback === 'function') callback();
+    componentWillMount = () => {
+        this.setAuthMenu();
+    }
+
+    componentDidMount = () => {
+        this.selectActiveTab();
+
+        window.addEventListener('hashchange', () => {
+            this.selectActiveTab();
         });
-        return;
-      }
     }
-  }
 
-  getFlatMenu = menu => {
-    return menu.reduce((keys, item) => {
-      keys.push(item);
-      if (item.children) {
-        return keys.concat(this.getFlatMenu(item.children));
-      }
-      return keys;
-    }, []);
-  }
+    componentWillReceiveProps = nextProps => {
+    }
 
-  setMenuChildren = () => {
-    const {selectedKeys, authMenu} = this.state;
-    const subMenuList = authMenu.map(item => {
-      if (item.children) {
-        return (
-          <SubMenu
-            key={item.key}
-            title={<span><Icon type={item.iconType}/><span>{item.label}</span></span>}
-          >
-            {
-              item.children.map(subItem => {
-                return (
-                  <Menu.Item key={subItem.key}>
-                    <Link to={subItem.link}>{subItem.label}</Link>
-                  </Menu.Item>
-                )
-              })
+    setAuthMenu = () => {
+        if (sessionStorage.type !== undefined && sessionStorage.type !== null) {
+            const type = sessionStorage.type;
+            let authority_menu = [];
+            if (type === "1")
+                authority_menu = admin;
+            else if (type === "2") {
+                authority_menu = subAdmin;
             }
-          </SubMenu>
-        )
-      } else {
+            else if (type === "3") {
+                authority_menu = operator;
+            }
+            let _menu = [];
+            menuTree.map(item => {
+                const _item = {};
+                for (let i = 0; i < authority_menu.length; i++) {
+                    if (item.key === authority_menu[i].key) {
+                        _item.key = item.key;
+                        _item.iconType = item.iconType;
+                        _item.label = item.label;
+                        _item.children = [];
+                        authority_menu[i].children.map(sub_key => {
+                            _item.children.push(_.find(item.children, {key: sub_key}));
+                        });
+                        _menu.push(_item);
+                    }
+                }
+            });
+            console.log("authMenu ===", _menu);
+            this.setState({authMenu: _menu});
+        }
+    }
+
+    selectActiveTab = () => {
+        const menu = this.getFlatMenu(this.state.authMenu);
+        const hashUrl = location.hash.split('#')[1];
+        for (let i = 0; i < menu.length; i++) {
+            const item = menu[i];
+            if (hashUrl.indexOf(item.link) > -1) {
+                this.setState({selectedKeys: item.key});
+                return;
+            }
+        }
+    }
+
+    getFlatMenu = menu => {
+        return menu.reduce((keys, item) => {
+            keys.push(item);
+            if (item.children) {
+                return keys.concat(this.getFlatMenu(item.children));
+            }
+            return keys;
+        }, []);
+    }
+
+    setMenuChildren = () => {
+        const {selectedKeys, authMenu} = this.state;
+        const subMenuList = authMenu.map(item => {
+            if (item.children) {
+                return (
+                    <SubMenu
+                        key={item.key}
+                        title={<span><Icon type={item.iconType}/><span>{item.label}</span></span>}
+                    >
+                        {
+                            item.children.map(subItem => {
+                                return (
+                                    <Menu.Item key={subItem.key}>
+                                        <Link to={subItem.link}>{subItem.label}</Link>
+                                    </Menu.Item>
+                                )
+                            })
+                        }
+                    </SubMenu>
+                )
+            } else {
+                return (
+                    <Menu.Item key={item.key}>
+                        <Link to={item.link}>
+                            <Icon type={item.iconType}/>
+                            <span>{item.label}</span>
+                        </Link>
+                    </Menu.Item>
+                )
+            }
+        });
+
         return (
-          <Menu.Item key={item.key}>
-            <Link to={item.link}>
-              <Icon type={item.iconType}/>
-              <span>{item.label}</span>
-            </Link>
-          </Menu.Item>
-        )
-      }
-    });
+            <Menu
+                theme="dark"
+                mode="inline"
+                selectedKeys={[selectedKeys]}
+                defaultSelectedKeys={['1_1']}
+                defaultOpenKeys={['1', '2', '3', '4', '5']}
+            >{subMenuList}</Menu>
+        );
+    }
 
-    return (
-      <Menu
-        theme="dark"
-        mode="inline"
-        selectedKeys={[selectedKeys]}
-        defaultSelectedKeys={['1_1']}
-        defaultOpenKeys={['1', '2', '3', '4', '5']}
-      >{subMenuList}</Menu>
-    );
-  }
+    onClick = e => {
+        this.setState({
+            selectedKeys: e.key
+        });
+    }
 
-  onClick = e => {
-    this.setState({
-      selectedKeys: e.key
-    });
-  }
+    render() {
+        const {selectedKeys, subMenuList} = this.state;
+        const {collapsed} = this.props;
+        console.log(' ========================= ');
 
-  render() {
-    const {selectedKeys, subMenuList} = this.state;
-    const {collapsed} = this.props;
-    console.log(' ========================= ');
-
-    return (
-      <Sider
-        trigger={null}
-        collapsible
-        collapsed={collapsed}
-        width={256}
-        className="left-side"
-      >
-        <div className="logo">
-          <Link to="/frame/user/list">
-            <h1>ADMIN</h1>
-          </Link>
-        </div>
-        <Scrollbars style={{height: 'calc(100vh - 64px)'}}>
-          {this.setMenuChildren()}
-        </Scrollbars>
-      </Sider>
-    );
-  }
+        return (
+            <Sider
+                trigger={null}
+                collapsible
+                collapsed={collapsed}
+                width={256}
+                className="left-side"
+            >
+                <div className="logo">
+                    <Link to="/frame/user/list">
+                        <h1>ADMIN</h1>
+                    </Link>
+                </div>
+                <Scrollbars style={{height: 'calc(100vh - 64px)'}}>
+                    {this.setMenuChildren()}
+                </Scrollbars>
+            </Sider>
+        );
+    }
 }
 
 ZZLeftSide.contextTypes = {
-  router: PropTypes.object
+    router: PropTypes.object
 }
 
 export default ZZLeftSide;
--- src/modules/login/component/login.js
+++ src/modules/login/component/login.js
@@ -13,121 +13,121 @@ const loginUrl = restUrl.BASE_HOST + 'user/login';
 const roleDetailUrl = restUrl.BASE_HOST + 'role/findbyid';
 
 class Login extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      loading: false
-    };
-  }
-
-  componentDidMount = () => {
-    sessionStorage.clear();
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        this.setState({
-          loading: true
-        });
-        ajax.postJSON(loginUrl, JSON.stringify(values), (data) => {
-          if (data.success) {
-            let backData = data.backData;
-            sessionStorage.setItem('token', backData.token);
-            const loginedUser = backData.loginedUser;
-            if (loginedUser) {
-              ajax.getJSON(roleDetailUrl, {id: loginedUser.roleId }, (data) => {
-                if (data.success) {
-                  const role = data.backData;
-                  let type = 1;
-                  if(role.roleCode === '003')
-                    type = 2;
-                  else if(role.roleCode === '004')
-                    type = 3;
-                  sessionStorage.setItem('type', type);
-                }
-              })
-              sessionStorage.setItem('expireDate', new Date(new Date().getTime() + 10000000));
-              sessionStorage.setItem('userId', loginedUser.id);
-              sessionStorage.setItem('userName', loginedUser.userName);
-              sessionStorage.setItem('roleId', loginedUser.roleId);
-              if (loginedUser.assessorys && loginedUser.assessorys.length > 0) {
-                sessionStorage.setItem('avatar', restUrl.ADDR + loginedUser.assessorys[0].path + loginedUser.assessorys[0].name);
-              }
-            }
-
-            return this.context.router.push('/frame/user/list');
-          } else {
-            Message.error(data.backMsg);
-          }
-          this.setState({
+        this.state = {
             loading: false
-          });
+        };
+    }
+
+    componentDidMount = () => {
+        sessionStorage.clear();
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                this.setState({
+                    loading: true
+                });
+                ajax.postJSON(loginUrl, JSON.stringify(values), (data) => {
+                    if (data.success) {
+                        let backData = data.backData;
+                        sessionStorage.setItem('token', backData.token);
+                        const loginedUser = backData.loginedUser;
+                        if (loginedUser) {
+                            ajax.getJSON(roleDetailUrl, {id: loginedUser.roleId}, res => {
+                                if (res.success) {
+                                    const role = res.backData;
+                                    let type = 1;
+                                    if (role.roleCode === '003')
+                                        type = 2;
+                                    else if (role.roleCode === '004')
+                                        type = 3;
+                                    sessionStorage.setItem('type', type);
+
+                                    return this.context.router.push('/frame/user/list');
+                                }
+                            })
+                            sessionStorage.setItem('expireDate', new Date(new Date().getTime() + 10000000));
+                            sessionStorage.setItem('userId', loginedUser.id);
+                            sessionStorage.setItem('userName', loginedUser.userName);
+                            sessionStorage.setItem('roleId', loginedUser.roleId);
+                            if (loginedUser.assessorys && loginedUser.assessorys.length > 0) {
+                                sessionStorage.setItem('avatar', restUrl.ADDR + loginedUser.assessorys[0].path + loginedUser.assessorys[0].name);
+                            }
+                        }
+                    } else {
+                        Message.error(data.backMsg);
+                    }
+                    this.setState({
+                        loading: false
+                    });
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
-  render() {
-    const {getFieldDecorator} = this.props.form;
+    render() {
+        const {getFieldDecorator} = this.props.form;
 
-    return (
-      <div className="login">
-        <img src={loginBg} className="login-bg"/>
-        <div className="backup"></div>
-        <div className="login-box">
-          <Row>
-            <Col span={13} style={{height: '400px', backgroundColor: 'rgba(101, 116, 154, .65)'}}>
-              <div style={{margin: '85px 0 40px', textAlign: 'center'}}>
-                <Icon type="windows" theme='filled' style={{fontSize: 90, color: '#fff'}}/>
-              </div>
-              <div
-                style={{paddingTop: 30, textAlign: 'center', fontSize: 20, color: '#fff'}}>订单管理后台系统
-              </div>
-            </Col>
-            <Col span={11} style={{height: '400px', padding: '20px 35px', backgroundColor: '#fff'}}>
-              <Form onSubmit={this.handleSubmit} className="login-form">
-                <FormItem>
-                  <h1 style={{margin: '15px 0 0 0'}}>欢迎登录</h1>
-                  <p style={{margin: '0', color: '#999'}}>Welcome!</p>
-                </FormItem>
-                <FormItem>
-                  {getFieldDecorator('loginName', {
-                    rules: [{required: true, message: '请输入您的用户名!'}],
-                  })(
-                    <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                           className="login-input" placeholder="用户编码/手机号"/>
-                  )}
-                </FormItem>
-                <FormItem>
-                  {getFieldDecorator('password', {
-                    rules: [{required: true, message: '请输入您的密码!'}],
-                  })(
-                    <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                           type="password" className="login-input" placeholder="密码"/>
-                  )}
-                </FormItem>
-                <FormItem>
-                  <Button type="primary" htmlType="submit" className="login-form-button"
-                          loading={this.state.loading}>
-                    登录
-                  </Button>
-                </FormItem>
-              </Form>
-            </Col>
-          </Row>
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div className="login">
+                <img src={loginBg} className="login-bg"/>
+                <div className="backup"></div>
+                <div className="login-box">
+                    <Row>
+                        <Col span={13} style={{height: '400px', backgroundColor: 'rgba(101, 116, 154, .65)'}}>
+                            <div style={{margin: '85px 0 40px', textAlign: 'center'}}>
+                                <Icon type="windows" theme='filled' style={{fontSize: 90, color: '#fff'}}/>
+                            </div>
+                            <div
+                                style={{paddingTop: 30, textAlign: 'center', fontSize: 20, color: '#fff'}}>订单管理后台系统
+                            </div>
+                        </Col>
+                        <Col span={11} style={{height: '400px', padding: '20px 35px', backgroundColor: '#fff'}}>
+                            <Form onSubmit={this.handleSubmit} className="login-form">
+                                <FormItem>
+                                    <h1 style={{margin: '15px 0 0 0'}}>欢迎登录</h1>
+                                    <p style={{margin: '0', color: '#999'}}>Welcome!</p>
+                                </FormItem>
+                                <FormItem>
+                                    {getFieldDecorator('loginName', {
+                                        rules: [{required: true, message: '请输入您的用户名!'}],
+                                    })(
+                                        <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                               className="login-input" placeholder="用户编码/手机号"/>
+                                    )}
+                                </FormItem>
+                                <FormItem>
+                                    {getFieldDecorator('password', {
+                                        rules: [{required: true, message: '请输入您的密码!'}],
+                                    })(
+                                        <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                                               type="password" className="login-input" placeholder="密码"/>
+                                    )}
+                                </FormItem>
+                                <FormItem>
+                                    <Button type="primary" htmlType="submit" className="login-form-button"
+                                            loading={this.state.loading}>
+                                        登录
+                                    </Button>
+                                </FormItem>
+                            </Form>
+                        </Col>
+                    </Row>
+                </div>
+            </div>
+        );
+    }
 }
 
 const WrappedLogin = Form.create()(Login);
 
 Login.contextTypes = {
-  router: PropTypes.object
+    router: PropTypes.object
 }
 
 export default WrappedLogin;
